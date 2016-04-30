b80c574cc0ecb10395bd8797490f6a632d8998a8
Chad Curtis <rampantprint@gmail.com>
2016-04-30 17:47:12 -0500

    Remove dynamic title.

--- header.php
+++ header.php
@@ -33,9 +33,9 @@
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">C. W. Curtis</a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">C. W. Curtis</a></p>
 			<?php
 			endif;
 

730f04895326822f7022fcd7a20844fa4c5df9da
Chad Curtis <rampantprint@gmail.com>
2016-04-30 13:35:25 -0500

    Initial commit.

---
+++ template-parts/content-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package rampantprint
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'rampantprint' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->

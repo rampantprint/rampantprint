6c625e2a083b6a058f622c8b801e8128ee61c4e4
Chad Curtis <rampantprint@gmail.com>
2017-01-15 20:57:53 -0600

    Add Magnific Popup.

--- functions.php
+++ functions.php
@@ -127,6 +127,20 @@ function rampantprint_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'rampantprint_scripts' );
 
+add_action('wp_enqueue_scripts', 'enqueue_magnificpopup_styles');
+function enqueue_magnificpopup_styles() {
+    wp_register_style('magnific_popup_style', get_stylesheet_directory_uri().'/vendor/magnific-popup/magnific-popup.css', array('rampantprint-style'));
+    wp_enqueue_style('magnific_popup_style');
+}
+ 
+add_action('wp_enqueue_scripts', 'enqueue_magnificpopup_scripts');
+function enqueue_magnificpopup_scripts() {
+    wp_register_script('magnific_popup_script', get_stylesheet_directory_uri().'/vendor/magnific-popup/jquery.magnific-popup.min.js', array('jquery'));
+    wp_enqueue_script('magnific_popup_script');
+    wp_register_script('magnific_init_script', get_stylesheet_directory_uri().'/vendor/magnific-popup/jquery.magnific-popup-init.js', array('jquery'));
+    wp_enqueue_script('magnific_init_script');
+}
+
 /**
  * Enqueue Google Fonts
 */
